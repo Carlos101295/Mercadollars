91a1c934907f82d41ce964ce2e0d68daa918eff9
Carlos101295 <cdr0017@alu.medac.es>
2025-12-02 09:14:11 +0100

    Delete Trigger Historico MERCADOLLARS.sql

--- Trigger Historico MERCADOLLARS.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER $$
-CREATE TRIGGER TG_HISTORICO_VENTAS
-AFTER INSERT ON VENTAS FOR EACH ROW
-BEGIN
-	INSERT INTO historico_ventas VALUES (NEW.Productos_idProductos, new.Clientes_idClientes, new.productos.nombre, new.productos.grupo/tipo, new.productos.precio_sin_IVA, new.productos.IVA_aplicable, new.productos.precio_final, new.ticket, new.fecha_venta);
-END;$$
-
-DELIMITER ;
