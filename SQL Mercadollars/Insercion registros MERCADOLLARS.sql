6664f84b96b556c894dd0c4504aee28560afac2e
Carlos101295 <cdr0017@alu.medac.es>
2025-12-02 09:19:39 +0100

    Add files via upload

---
+++ SQL Mercadollars/Insercion registros MERCADOLLARS.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCTOS VALUES (1,'leche','lacteos', 10, null, 5, 1.20, 4, 1.30, null);
+INSERT INTO CLIENTES VALUES (2, 'eufrasio', null, null, null, default);
+INSERT INTO USUARIO VALUES (3, 'empleadito1', null, null, default, 'empleadito1@gmail.com', null, default);
+INSERT INTO VENTAS VALUES (2, 1, 3, current_timestamp(), 123456789);
